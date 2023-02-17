c64b9a268e05c5fd123988f049d6170028d028c0
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-02-17 08:01:25 -0800

    adding a skeleton fo the data types hw

---
+++ DIY_data_types.rb
@@ -0,0 +1,65 @@
+class Stack
+
+    def initialize()
+
+    end
+
+    def push(el)
+
+    end
+
+    def pop
+
+    end
+
+    def peek
+
+    end
+
+end
+
+
+class Queue
+
+    def initialize()
+
+    end
+
+    def enque(el)
+
+    end
+
+    def deque
+
+    end
+
+    def peek
+
+    end
+
+end
+
+
+class Map
+
+    def initialize()
+
+    end
+
+    def get(key)
+
+    end
+
+    def set(key, value)
+
+    end
+
+    def delete(key)
+
+    end
+
+    def show
+
+    end
+
+end
